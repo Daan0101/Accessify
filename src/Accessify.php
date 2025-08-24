a965a4f7bdfafd254cffb467c1eb0471a86104bb
Daan Schouten <173400@student.talland.nl>
2025-08-24 10:02:07 -0700

    feat: generate code

--- src/Accessify.php
+++ src/Accessify.php
@@ -3,6 +3,7 @@
 namespace Daan0101\Accessify;
 
 use Illuminate\Support\Facades\Log;
+use \Illuminate\Support\Str;
 
 class Accessify
 {
@@ -10,5 +11,12 @@ class Accessify
     {
         return '1.0.0';
         \Log::info('Accessify version ' . self::getAccessifyVersion() . ' accessed.');
+    }
+
+    public static function Generate(): string
+    {
+        $code = Str::random(25);
+        \Log::info('Code generated: ' . $code);
+        return $code;
     }
 }
